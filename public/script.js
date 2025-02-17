6e9bf38116e32bb3c68eeb2fae8ed07358914670
IICruxoII <cruxo1981@gmail.com>
2025-02-17 14:20:04 +0100

    modificacion

--- public/script.js
+++ public/script.js
@@ -7,7 +7,7 @@ document.getElementById("registerForm").addEventListener("submit", async (event)
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
-    const response = await fetch("http://localhost:3000/register", {
+    const response = await fetch("https://examen-sc91.onrender.com/", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, email, password }),
